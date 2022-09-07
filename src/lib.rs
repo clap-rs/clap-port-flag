74af9d18911b536d8980108933ca7ef3c7fb7075
Matthias Beyer <mail@beyermatthias.de>
2022-09-07 09:11:27 +0200

    Add feature to have --address be a SocketAddr
    When this feature is enabled, the --port flag is disabled and
    --address takes a std::net::SocketAddr, which can be parsed from a
    string like "127.0.0.1:8080" (including the port number).
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(test, deny(warnings))]
 
-use std::io;
 use std::net::TcpListener;
 use std::os::raw::c_int;
 use std::os::unix::io::FromRawFd;
@@ -21,12 +20,21 @@ use std::os::unix::io::FromRawFd;
 /// ```
 #[derive(clap::Args, Debug)]
 pub struct Port {
+    /// The network address and port to listen to.
+    #[cfg(feature = "addr_with_port")]
+    #[clap(short = 'a', long = "address", default_value = "127.0.0.1:80")]
+    address: std::net::SocketAddr,
+
     /// The network address to listen to.
+    #[cfg(not(feature = "addr_with_port"))]
     #[clap(short = 'a', long = "address", default_value = "127.0.0.1")]
     address: String,
+
     /// The network port to listen to.
+    #[cfg(not(features = "addr_with_port"))]
     #[clap(short = 'p', long = "port", env = "PORT", group = "bind")]
     port: Option<u16>,
+
     /// A previously opened network socket.
     #[clap(long = "listen-fd", env = "LISTEN_FD", group = "bind")]
     fd: Option<c_int>,
@@ -41,12 +49,26 @@ pub struct Port {
 impl Port {
     /// Create a TCP socket from the passed in port or file descriptor.
     pub fn bind(&self) -> std::io::Result<TcpListener> {
-        match self {
-            Self { fd: Some(fd), .. } => unsafe { Ok(TcpListener::from_raw_fd(*fd)) },
-            Self {
-                port: Some(port), ..
-            } => TcpListener::bind((self.address.as_str(), *port)),
-            _ => Err(io::Error::new(io::ErrorKind::Other, "No port supplied.")),
+        if let Some(fd) = self.fd {
+            return unsafe { Ok(TcpListener::from_raw_fd(fd)) };
+        }
+
+        #[cfg(feature = "addr_with_port")]
+        {
+            let addr: std::net::SocketAddr = self.address;
+            TcpListener::bind(addr)
+        }
+        #[cfg(not(feature = "addr_with_port"))]
+        {
+            let addr: &str = self.address.as_str();
+            if let Some(port) = self.port {
+                TcpListener::bind((addr, port))
+            } else {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "No port supplied.",
+                ))
+            }
         }
     }
 
@@ -54,6 +76,19 @@ impl Port {
     /// on `port`.
     ///
     /// Useful to create a default socket to listen to if none was passed.
+    #[cfg(feature = "addr_with_port")]
+    pub fn bind_or(&self, port: u16) -> std::io::Result<TcpListener> {
+        let mut addr = self.address;
+        addr.set_port(port);
+
+        self.bind().or_else(|_| TcpListener::bind(addr))
+    }
+
+    /// Create a TCP socket by calling to `.bind()`. If it fails, create a socket
+    /// on `port`.
+    ///
+    /// Useful to create a default socket to listen to if none was passed.
+    #[cfg(not(feature = "addr_with_port"))]
     pub fn bind_or(&self, port: u16) -> std::io::Result<TcpListener> {
         self.bind()
             .or_else(|_| TcpListener::bind((self.address.as_str(), port)))
